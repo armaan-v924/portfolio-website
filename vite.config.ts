7d0a0169a08f6c838fb572a069a5ce3305cabc48
Armaan Vakharia <43391096+armaan-v924@users.noreply.github.com>
2025-02-17 12:10:32 -0600

    Add vite-plugin-radar for analytics integration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,16 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import svgr from "vite-plugin-svgr"
 
+import { VitePluginRadar } from "vite-plugin-radar"
+
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(),
+    VitePluginRadar({
+      analytics: {
+        id: "G-P0S724ZBJ6"
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
